1e3f73ade3ef5f7cc5cd58036f5bf86eb007c155
Gburis <gabrielleiteolimpio@gmail.com>
2019-08-13 11:02:12 -0300

    implementação de metodos POST, GET, PUT, DELETE

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
-module.exports = function(app,db){
+
+module.exports = function(app, db, objectId){
     app.get('/', function(req, res){
         res.send({msg:'Olá'});
     });
@@ -8,14 +9,85 @@ module.exports = function(app,db){
         db.open(function(err, mongoclient){
             mongoclient.collection('postagens', function(err, collection){
                 collection.insert(data, function(err, records){
-                    if(err){
-                        res.json(err);
-                    }else{
-                        res.json(records);
-                    }
+
+                    !err ? res.json(records) : res.json(err);
+                    
                     mongoclient.close();
                 });
             });
         });
     });
+
+    app.get('/api', function(req, res){
+    
+        db.open(function(err, mongoclient){
+            mongoclient.collection('postagens', function(err, collection){
+                collection.find().toArray(function(err, resul){
+
+                    !err ? res.json(resul) : res.json(err);
+
+                    mongoclient.close();
+                });
+            });
+        });
+    });
+
+    app.get('/api/:id', function(req, res){
+
+        let params = req.params.id;
+
+        db.open(function(err, mongoclient){
+            mongoclient.collection('postagens', function(err, collection){
+                collection.find(objectId(params)).toArray(function(err, resul){
+
+                    !err ? res.json(resul) : res.json(err);
+
+                    mongoclient.close();
+                });
+            });
+        });
+    });
+
+    app.put('/api/:id', function(req, res){
+
+        let params = req.params.id;
+
+        db.open(function(err, mongoclient){
+            mongoclient.collection('postagens', function(err, collection){
+            
+                collection.update(
+                    { _id: objectId(params) },
+                    { $set:{name: req.body.name}},
+                    {},
+                    function(err, records){
+
+                        !err ? res.json(records) : res.json(err);
+
+                        mongoclient.close();
+                    }
+                );
+            });
+        });
+    });
+
+    app.delete('/api/:id', function(req, res){
+
+        let params = req.params.id;
+
+        db.open(function(err, mongoclient){
+            mongoclient.collection('postagens', function(err, collection){
+            
+                collection.remove(
+                    {_id: objectId(params)},
+                    function(err, records){
+
+                        !err ? res.json(records) : res.json(err);
+
+                        mongoclient.close();
+                    }
+                );
+            });
+        });
+    });
+    
 }
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongodb = require('mongodb');
-
+var objectId = require('mongodb').ObjectId
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,11 +13,11 @@ app.listen(8080);
 
 var db = new mongodb.Db(
     'testes',
-    new mongodb.Server('kit-corretor-axa-dev.onibusiness.com.br' , 27017, {}),
+    new mongodb.Server('localhost' , 23456, {}),
     {}
 );
 
-var routesApi = require('./routes/routes')(app, db);
+var routesApi = require('./routes/routes')(app, db, objectId);
 
 console.log('Servidor On');
 

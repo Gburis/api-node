1981e9ba877e681f2185a4e04e8c7c53b3b30816
Gburis <gabrielleiteolimpio@gmail.com>
2019-08-13 15:42:10 -0300

    Validações de dados de entrada

--- routes/routes.js
+++ routes/routes.js
@@ -1,16 +1,29 @@
+const { check, validationResult } = require('express-validator');
 
 module.exports = function(app, db, objectId){
     app.get('/', function(req, res){
-        res.send({msg:'Olá'});
+        res.send('api');
     });
 
-    app.post('/api', function(req, res){
+    app.post('/api', [
+        check('name').not().isEmpty().withMessage('name is not null'),
+        check('image').not().isEmpty().withMessage('image is not null'),
+
+    ],function(req, res){
+
+        let errors = validationResult(req);
+        let arr = errors.array();
+        if(!errors.isEmpty()){
+            res.status(400).json({error: arr[0].msg});
+            return false;
+        }
+
         let data = req.body;
         db.open(function(err, mongoclient){
             mongoclient.collection('postagens', function(err, collection){
                 collection.insert(data, function(err, records){
 
-                    !err ? res.json(records) : res.json(err);
+                    !err ? res.status(200).json(records) : res.status(500).json(err);
                     
                     mongoclient.close();
                 });
@@ -24,7 +37,8 @@ module.exports = function(app, db, objectId){
             mongoclient.collection('postagens', function(err, collection){
                 collection.find().toArray(function(err, resul){
 
-                    !err ? res.json(resul) : res.json(err);
+
+                    !err ? res.status(200).json(resul) : res.status(500).json(err);
 
                     mongoclient.close();
                 });
@@ -35,12 +49,17 @@ module.exports = function(app, db, objectId){
     app.get('/api/:id', function(req, res){
 
         let params = req.params.id;
+        
+        if(params.length != 24){
+            res.json({error: '24 hex characters'});
+            return false;
+        }
 
         db.open(function(err, mongoclient){
             mongoclient.collection('postagens', function(err, collection){
-                collection.find(objectId(params)).toArray(function(err, resul){
-
-                    !err ? res.json(resul) : res.json(err);
+                collection.find({ _id :objectId(params)}).toArray(function(err, resul){
+                    
+                    !err ? res.status(200).json(resul) : res.status(500).json(err);
 
                     mongoclient.close();
                 });
@@ -52,6 +71,11 @@ module.exports = function(app, db, objectId){
 
         let params = req.params.id;
 
+        if(params.length != 24){
+            res.json({error: '24 hex characters'});
+            return false;
+        }
+
         db.open(function(err, mongoclient){
             mongoclient.collection('postagens', function(err, collection){
             
@@ -61,7 +85,7 @@ module.exports = function(app, db, objectId){
                     {},
                     function(err, records){
 
-                        !err ? res.json(records) : res.json(err);
+                        !err ? res.status(200).json(records) : res.status(500).json(err);
 
                         mongoclient.close();
                     }
@@ -74,6 +98,11 @@ module.exports = function(app, db, objectId){
 
         let params = req.params.id;
 
+        if(params.length != 24){
+            res.json({error: '24 hex characters'});
+            return false;
+        }
+        
         db.open(function(err, mongoclient){
             mongoclient.collection('postagens', function(err, collection){
             
@@ -81,7 +110,7 @@ module.exports = function(app, db, objectId){
                     {_id: objectId(params)},
                     function(err, records){
 
-                        !err ? res.json(records) : res.json(err);
+                        !err ? res.status(200).json(records) : res.status(400).json(err);
 
                         mongoclient.close();
                     }
